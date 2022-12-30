66d4ecfb3e58fd43f0d07421517a78ffc6ec7e80
Sem Kholodov <kholodovsem@gmail.com>
2022-12-30 22:29:19 +0200

    finished

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import "./style.css";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./formik/App";
+import './style.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './Project/App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
